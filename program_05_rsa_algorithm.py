a29a68f6ca20313414a100642d2b41eed35c07f2
Dass-CSE <dasscse2008@gmail.com>
2025-07-18 09:09:03 +0530

    Update program_05_rsa_algorithm.py

--- program_05_rsa_algorithm.py
+++ program_05_rsa_algorithm.py
@@ -2,8 +2,8 @@ p, q = 13, 17
 n = p * q
 phi = (p-1)*(q-1)
 e = 5
-d = pow(e, -1, phi)
+d = pow(e, -1, phi) #Modular inverse
 msg = 10
-cipher = pow(msg, e, n)
-plain = pow(cipher, d, n)
+cipher = pow(msg, e, n) #Encrypt
+plain = pow(cipher, d, n) # Decrypt
 print("Decrypted:", plain)
